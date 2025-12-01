90b2be9163ce95696b16408eecd219a53673841f
Tomascod <tomas.kodydek@infis.cz>
2025-12-01 10:33:20 +0100

    základní soubor

---
+++ src/Retezce/Retezce.java
@@ -0,0 +1,21 @@
+package Retezce;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Retezce {
+    public static void main(String[] args) {
+        String a = null;
+
+
+        if ("".equals(a)) System.out.println("ano");
+        else System.out.println("ne");
+
+        //.equals - jestli se 2 řetězce rovnají
+        //a.length() - vrací délku řetězce
+        //IgnoreCase - ignoruje malá a velká písmena
+        //.isEmpty - zjistí zda je řetězec prázdné
+        //null je naprd při porovnávání, protože to často dá chybovou hlášku - řešení - "".equals()
+        //když mam 2 proměnné, ale jedna z nich je deklarovaná String a = new String ("x"), tak se to nerovná
+        //.toLowerCase()/.toUpperCase()
+    }
+}
