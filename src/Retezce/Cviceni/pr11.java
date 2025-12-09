29246cc9c3ed310212ced0ed481382dfe2eac01d
Tomaskod <tomas.kodydek@infis.cz>
2025-12-09 17:45:41 +0100

    hotovo

--- src/Retezce/Cviceni/pr11.java
+++ src/Retezce/Cviceni/pr11.java
@@ -3,6 +3,11 @@ package Retezce.Cviceni;
 public class pr11 {
     public static void main(String[] args) {
         String palindrom = "oko";
+        String obraceny = new StringBuilder(palindrom).reverse().toString();
+
+        if(palindrom.equals(obraceny)){
+            System.out.println("Je to palindrom");
+        }else System.out.println("není palindrom");
 
 
     }
--- src/Retezce/Cviceni/pr12.java
+++ src/Retezce/Cviceni/pr12.java
@@ -3,5 +3,25 @@ package Retezce.Cviceni;
 public class pr12 {
     public static void main(String[] args) {
         String text = "Ahoj";
+        char[] abeceda = {
+                'A','B','C','D','E','F','G','H','I','J',
+                'K','L','M','N','O','P','Q','R','S','T',
+                'U','V','W','X','Y','Z'
+        };
+
+        String[] morseovka = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
+        };
+
+        String slovoPredelane = "";
+
+        for (int i = 0; i <= text.length()-1; i++) {
+            for (int j = 0; j <= abeceda.length-1; j++) {
+                if ( text.toUpperCase().charAt(i)==abeceda[j]) {
+                    slovoPredelane += morseovka[j];
+                }
+            }
+        }
+        System.out.println(slovoPredelane);
+
     }
 }
